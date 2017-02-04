350a18819e0865a06311879f6acba86f3944bbd7
Troy Alford <troy@troyalford.com>
2017-02-03 16:03:20 -0800

    Updating example

--- examples/src/example.js
+++ examples/src/example.js
@@ -3,9 +3,15 @@ import * as ReactDOM from 'react-dom'
 
 import TreeView from '../../src/components/TreeView'
 
+const log = document.getElementById('log')
+
 ReactDOM.render(
   <TreeView
-    onNodeClick={(e, node) => { e.stopPropagation(); console.log('Clicked:', node) }}
+    onNodeClick={(e, node) => {
+      e.stopPropagation()
+      log.append('Clicked:', JSON.stringify(node))
+      log.appendChild(document.createElement('br'))
+    }}
     nodes={[{
       text: 'Home',
       url:  '#',
